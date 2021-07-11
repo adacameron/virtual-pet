61d0a3fe7f98fddc1dd5ab99956584728a111662
adacameron <ada.e.cameron@gmail.com>
2021-07-11 17:01:09 +0100

    set name property

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,4 +4,10 @@ describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Gerald')).toBeInstanceOf(Object);
     });
+
+    it('sets the name property', () => {
+      const pet = new Pet('Gerald');
+
+      expect(pet.name).toEqual('Gerald');
+    });
   });
