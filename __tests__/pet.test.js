90765632eb776fda5cbd541d5d11b259090deaa8
adacameron <ada.e.cameron@gmail.com>
2021-07-11 17:31:06 +0100

    added growUp method on Pet prototype

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,13 +1,28 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    it('returns an object', () => {
-      expect(new Pet('Gerald')).toBeInstanceOf(Object);
-    });
+  it('returns an object', () => {
+    expect(new Pet('Gerald')).toBeInstanceOf(Object);
+  });
 
-    it('sets the name property', () => {
+  it('sets the name property', () => {
+    const pet = new Pet('Gerald');
+
+    expect(pet.name).toEqual('Gerald');
+  });
+
+  it('has an initial age of 0', () => {
+    const pet = new Pet('Gerald');
+
+    expect(pet.age).toEqual(0);
+  });
+
+  describe('growUp', () => {
+    it('increments the age by 1', () => {
       const pet = new Pet('Gerald');
+      pet.growUp();
 
-      expect(pet.name).toEqual('Gerald');
+      expect(pet.age).toEqual(1);
     });
   });
+});
