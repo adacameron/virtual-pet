b2256215cba7e6f4a31019085991c3cde92cd296
adacameron <ada.e.cameron@gmail.com>
2021-07-12 14:23:03 +0100

    added walk & feed functions

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -19,38 +19,67 @@ describe('constructor', () => {
 
   it('has an initial hunger of 0', () => {
     const pet = new Pet('Gerald');
-    
+
     expect(pet.hunger).toEqual(0);
   });
 
   it('has an initial fitness of 10', () => {
     const pet = new Pet('Gerald');
-    
-    expect(pet.fitness).toEqual(10); 
-  })
+
+    expect(pet.fitness).toEqual(10);
+  });
 });
 
-  describe('growUp', () => {
-    it('increments the age by 1', () => {
-      const pet = new Pet('Gerald');
-      pet.growUp();
+describe('growUp', () => {
+  it('increments the age by 1', () => {
+    const pet = new Pet('Gerald');
+    pet.growUp();
 
-      expect(pet.age).toEqual(1);
-    });
-
-    it('increments the hunger by 5', () => {
-      const pet = new Pet('Gerald');
-      pet.growUp();
-
-      expect(pet.hunger).toEqual(5);
-    });
-
-    it('decreases the fitness by 3', () => {
-      const pet = new Pet('Gerald');
-      pet.growUp();
-
-      expect(pet.fitness).toEqual(7);
-    })
+    expect(pet.age).toEqual(1);
   });
 
-  
+  it('increments the hunger by 5', () => {
+    const pet = new Pet('Gerald');
+    pet.growUp();
+
+    expect(pet.hunger).toEqual(5);
+  });
+
+  it('decreases the fitness by 3', () => {
+    const pet = new Pet('Gerald');
+    pet.growUp();
+
+    expect(pet.fitness).toEqual(7);
+  });
+});
+
+describe('walk', () => {
+  it('increments the fitness by 4, to a maximum of 10', () => {
+    const pet = new Pet('Gerald');
+
+    pet.fitness = 8;
+    pet.walk();
+
+    expect(pet.fitness).toEqual(10);
+  });
+});
+
+describe('feed', () => {
+  it('decreases the hunger level by 3', () => {
+    const pet = new Pet('Gerald');
+
+    pet.hunger = 6;
+    pet.feed();
+
+    expect(pet.hunger).toEqual(3);
+  });
+
+  it('decreases the hunger level to a minimum of 0', () => {
+    const pet = new Pet('Gerald');
+
+    pet.hunger = 2;
+    pet.feed();
+
+    expect(pet.hunger).toEqual(0);
+  })
+})
